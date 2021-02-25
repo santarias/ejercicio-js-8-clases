1e2e9f98c0b33f2eeaeec82bef80153ebd4e928d
Santiago Arias <santyac27@gmail.com>
2021-02-25 23:05:56 +0100

    Se crea clase Rey{}

--- js/index.js
+++ js/index.js
@@ -3,12 +3,14 @@ class Personaje {
   familia;
   edad;
   estado;
+  serie;
 
   constructor(nombreDefinir, familiaDefinir, edadDefinir) {
     this.nombre = nombreDefinir;
     this.familia = familiaDefinir;
     this.edad = edadDefinir;
     this.estado = "vivo";
+    this.serie = "Juego de Tronos";
   }
 
   morir() {
@@ -16,9 +18,20 @@ class Personaje {
   }
 
   comunicar() {
-    return `${this.nombre} dice: `;
+    return `${this.nombre} dice:`;
   }
 
 }
 
+class Rey extends Personaje {
+  anyosReinado;
+  constructor(nombre, familia, edad, reinado) {
+    super(nombre, familia, edad);
+    this.anyosReinado = reinado;
+  }
+
+  comunicar() {
+    return `${super.comunicar()} "Vais a Morir Todos"`
+  }
+}
 
