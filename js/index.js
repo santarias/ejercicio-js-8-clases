2f79fec6ee302bcf00aa6ad84dd42232d8f107df
Santiago Arias <santyac27@gmail.com>
2021-02-25 22:48:47 +0100

    Se crea clase Personaje

---
+++ js/index.js
@@ -0,0 +1,14 @@
+class Personaje {
+  nombre;
+  familia;
+  edad;
+  estado;
+
+  constructor(nombreDefinir, familiaDefinir, edadDefinir) {
+    this.nombre = nombreDefinir;
+    this.familia = familiaDefinir;
+    this.edad = edadDefinir;
+    this.estado = "vivo";
+  }
+}
+
