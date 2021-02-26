c5d5715fb0ee933be24311273656109257e5e88a
Santiago Arias <santyac27@gmail.com>
2021-02-26 11:42:50 +0100

    Se añade clase Escudero{}

--- js/index.js
+++ js/index.js
@@ -38,22 +38,60 @@ class Rey extends Personaje {
 class Luchador extends Personaje {
   arma;
   destreza;
-  constructor(nombre, familia, edad, armaDefinir) {
+  constructor(nombre, familia, edad, armaDefinir, destrezaDefinir) {
     super(nombre, familia, edad);
     this.arma = armaDefinir;
+    this.verificarDestreza(destrezaDefinir);
   }
   set setterDestreza(destrezaDefinir) {
-    if (destrezaDefinir > 0 && destrezaDefinir <= 10) {
-      this.destreza = destrezaDefinir;
-    } else if (destrezaDefinir < 0) {
+    this.verificarDestreza(destrezaDefinir);
+  }
+
+  verificarDestreza(destrezaVerificar) {
+    if (destrezaVerificar > 0 && destrezaVerificar <= 10) {
+      this.destreza = destrezaVerificar;
+    } else if (destrezaVerificar < 0) {
       this.destreza = 0;
-    } else if (destrezaDefinir > 10) {
+    } else if (destrezaVerificar > 10) {
       this.destreza = 10;
     }
   }
 
+
+
   comunicar() {
     return `${super.comunicar()} "Primero pego y luego pregunto"`;
+  }
+}
+class Escudero extends Personaje {
+  personajeQueSirve;
+  pelotismo;
+  constructor(nombre, familia, edad, personajeServir) {
+    super(nombre, familia, edad);
+    this.verificarPersonajeServir(personajeServir);
+  }
+  set personajeAServir(personajeDefinir) {
+    this.verificarPersonajeServir(personajeDefinir);
+  }
+  set gradoPelotismo(pelotismoVerificar) {
+    this.veriricarPelotismo(pelotismoVerificar);
+  }
+
+  verificarPersonajeServir(personaje) {
+    if (personaje instanceof Luchador) {
+      this.personajeQueSirve = personaje;
+    }
+  }
+
+  veriricarPelotismo(pelotismoDefinir) {
+    if (pelotismoDefinir > 0 && pelotismoDefinir <= 10) {
+      this.pelotismo = pelotismoDefinir;
+    } else {
+      this.pelotismo = 0;
+    }
+  }
+  comunicar() {
+    return `${super.comunicar()} "Soy un loser"`;
   }
 }
 
